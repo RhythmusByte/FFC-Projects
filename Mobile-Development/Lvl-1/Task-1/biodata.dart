20030611f0bf4d1598bcf3e082582360fc8a14c7
Akhil Mahesh <akhilmahesh@proton.me>
2024-09-05 18:34:26 +0530

    Updated Style

--- Mobile-Development/Lvl-1/Task-1/biodata.dart
+++ Mobile-Development/Lvl-1/Task-1/biodata.dart
@@ -1,51 +1,53 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Enter your name: ');
-  String? name = stdin.readLineSync();
+  // Collecting user data
+  stdout.write('Enter your first name: ');
+  String? fname = stdin.readLineSync();
+
+  stdout.write('Enter your last name: ');
+  String? lname = stdin.readLineSync();
+
   stdout.write('Enter your phone number: ');
-  String? phoneNumber = stdin.readLineSync();
+  String? number = stdin.readLineSync();
+
   stdout.write('Enter your age: ');
-  String? age = stdin.readLineSync();
-  stdout.write('Enter your height: ');
-  String? height = stdin.readLineSync();
-  stdout.write('Enter your weight: ');
-  String? weight = stdin.readLineSync();
+  int age = int.parse(stdin.readLineSync() ?? '0');
+
+  stdout.write('Enter your height (in cm): ');
+  double height = double.parse(stdin.readLineSync() ?? '0');
+
+  stdout.write('Enter your weight (in kg): ');
+  double weight = double.parse(stdin.readLineSync() ?? '0');
+
   stdout.write('Enter your address: ');
   String? address = stdin.readLineSync();
+
   stdout.write('Enter your hobbies: ');
-  String? hobbies = stdin.readLineSync();
+  List<String> hobbies = stdin.readLineSync()?.split(', ') ?? [];
 
-  print('\n Biodata');
-  print('---------\n');
-  print('Name: $name');
-  print('Phone Number: $phoneNumber');
-  print('Age: $age');
-  print('Height: $height cm');
-  print('Weight: $weight KG');
-  print('Address: $address');
-  print('Hobbies: $hobbies');
+  String hobbiesFormatted;
+
+  if (hobbies.isNotEmpty) {
+    hobbiesFormatted = hobbies.join(', ');
+  } else {
+    hobbiesFormatted = 'Hobbies not entered';
+  }
+
+  // Displaying Bio Data
+  print('''\n\n
+||=======================================
+||
+||      $fname's Bio Data
+||
+|| Name             $fname $lname
+|| Age              $age
+|| Phone Number     $number
+|| Height           ${height.toStringAsFixed(2)} cm
+|| Weight           ${weight.toStringAsFixed(2)} kg
+|| Address          $address
+|| Hobbies          $hobbiesFormatted
+||
+||=======================================\n
+''');
 }
-
-/*
-Output:
-
-Enter your name: Akhil
-Enter your phone number: +91 98765 43210
-Enter your age: 20
-Enter your height: 180.3
-Enter your weight: 25
-Enter your address: Kerala, India
-Enter your hobbies: Reading, Cooking, Music
-
- Biodata
----------
-
-Name: Akhil
-Phone Number: +91 98765 43210
-Age: 20
-Height: 180.3 cm
-Weight: 25 KG
-Address: Kerala, India
-Hobbies: Reading, Cooking, Music
-*/
